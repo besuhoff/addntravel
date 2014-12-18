25f76226935f2a33906ea1d7f75eefae0927ee0f
Albert Besuhoff <albert.besuhoff@gmail.com>
2014-12-18 02:55:57 +0200

    PR-II
    - Add mocks for records and tags

---
+++ src/javascript/app/mocks.js
@@ -0,0 +1,70 @@
+var mocks = mocks || {};
+mocks["records"] = [
+  {
+    "createdAt": "2011-06-14T11:52:56+02:00",
+    "updatedAt": "2012-12-12T15:35:15+01:00",
+    "id": 1,
+    "name": "València",
+    "googleId": "1sCjQqAAAAZa-W0sFDSRb2T1XZB3TbFWUS2FP0kQQCF66UtT8h63o1lzLrMnqx-FsEpQQhur_OEhBKhZ_hlsROGnqbVmikKKq5GhSvoGLujwT-elAjxyFOHmDXWaiMhw",
+    "tags": [{id: 3, value: 7}, {id: 4, value: '2015-01-01T12:00:00+01:00'}]
+  },
+
+  {
+    "createdAt": "2014-12-18T01:15:56+02:00",
+    "updatedAt": "2014-12-18T01:35:15+02:00",
+    "id": 2,
+    "title": "Statue of Liberty National Monument",
+    "googleId": "1sClROAAAALkO8g_kkW7eaBQqmcsyRBi5MPmBa2tnMmIqC01Qsbgo2f1Ov6NR-T7lC6Btut4sp2GqNjkbL9wPHJJR5Y_7XiM4pj_shPhk3YMKy7nqPAssSEIJzd3VHM_HWN_XSef7ykX4aFGD6_2tID1YswTtgG7bgLLAof-mw",
+    "tags": [{id: 1}, {id: 3, value: 1}, {id: 5, value: 2017}]
+  }
+
+];
+mocks["tags"] = [
+  {
+    "id" : 1,
+    "name":"beach",
+    "type": "plain"
+  },
+
+  {
+    "id" : 2,
+    "name":"mountains",
+    "type": "plain"
+  },
+
+  {
+    "id" : 3,
+    "name":"days",
+    "type": "number"
+  },
+
+  {
+    "id" : 4,
+    "name":"date",
+    "type": "datetime"
+  },
+
+  {
+    "id" : 5,
+    "name":"year",
+    "type": "number"
+  },
+
+  {
+    "id" : 5,
+    "name":"companions",
+    "type": "number"
+  },
+
+  {
+    "id" : 6,
+    "name":"sights",
+    "type": "plain"
+  },
+
+  {
+    "id" : 7,
+    "name":"shopping",
+    "type": "plain"
+  }
+];
